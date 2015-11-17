1e76825f93ff134d14a104bc7e05bfb798486d3a
Zae <526131722@qq.com>
2015-11-17 09:31:49 +0800

    改善打包脚本

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 # setup.py -- py to exe
 
 from distutils.core import setup
-import sys
-import glob
 import py2exe
-import os
+import zipfile
+import datetime
 
 
 options = {"py2exe": {"bundle_files": 1}}
@@ -12,3 +11,10 @@ options = {"py2exe": {"bundle_files": 1}}
 
 setup(console=["init_cmd.py"], options=options,
       zipfile=None, script_args=["py2exe"])
+
+date = datetime.datetime.now().strftime("[%y%m%d]")
+zipname = "dist/" + date + "FileTagger.zip"
+
+f = zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED)
+f.write("dist/init_cmd.exe", "FileTagger.exe")
+f.close()
