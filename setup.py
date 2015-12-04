cea4550e2b975a01b1277f6da7ebfb4124361daa
Zae <526131722@qq.com>
2015-12-04 09:08:52 +0800

    优化打包

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
-# setup.py -- py to exe
+# setup.py -- pack py to single exe
 
 from distutils.core import setup
 import py2exe
 import zipfile
 import datetime
 
+options = {
+    "compressed": 1,
+    "optimize": 2,
+    "ascii": 0,
+    "bundle_files": 1
+}
 
-options = {"py2exe": {"bundle_files": 1}}
 
-
-setup(console=["init_cmd.py"], options=options,
-      zipfile=None, script_args=["py2exe"])
+setup(
+    console=["init_cmd.py"],
+    options={'py2exe': options},
+    zipfile=None,
+    script_args=["py2exe"]
+)
 
 date = datetime.datetime.now().strftime("[%y%m%d]")
-zipname = "dist/" + date + "FileTagger.zip"
+zipname = "dist/" + date + "[C]FileTagger.zip"
 
 f = zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED)
 f.write("dist/init_cmd.exe", "FileTagger.exe")
